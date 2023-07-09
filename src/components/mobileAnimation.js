d9cc5747a3870ede999e253daec6cb09658eeb3b
Nikhil Toraskar <nikhil.toraskar@games24x7.com>
2023-07-09 18:52:22 +0530

    ui changes

--- src/components/mobileAnimation.js
+++ src/components/mobileAnimation.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import './mobileAnimation.css';
+import './animations.css';
 
 const MobileAnimation = (data) => {
 
@@ -7,7 +8,22 @@ const MobileAnimation = (data) => {
   return (
     <>
       <div className="mobileMockup">
-        
+        <div className="boundedMobile">
+          <div className="boundedWagonWheel">
+            <div class="pitch">
+              <img src={require("../static/groundBgNew.jpg")} alt="ground" />
+              <div class="ball">
+                <img src={require("../static/ballNew.png")} alt="ball" />
+              </div>
+              <div class="bat">
+                <img src={require("../static/batsman.png")} alt="ball" />
+              </div>
+            </div>
+          </div>
+          <div className="boundedAnimation">
+            <img src={require("../static/Umpire/1Run.gif")} alt="bowler" />
+          </div>
+        </div>
       </div>
     </>
   );
