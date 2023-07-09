131195af4e264ad19c25732ac24701aa607f8551
Neeva Ramoliya <neeva.ramoliya@games24x7.com>
2023-07-10 03:21:37 +0530

    integrate final code

--- python/python_app.py
+++ python/python_app.py
@@ -11,28 +11,7 @@ app.config.update(
     TEMPLATES_AUTO_RELOAD=True
 )
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-rows = [
-    {
-        "Default_Commentary": "47.5: Bilal Khan to Teja Nidamanuru, 2 runs.",
-        "Commentary": "Goes full this time and at the stumps. Teja Nidamanuru\u00a0lofts this back over the bowler's head and they run quickly to pick up a brace.",
-    },
-    {
-        "Default_Commentary": "47.4: Bilal Khan to Logan van Beek, Leg bye.",
-        "Commentary": "FOUR! Logan van Beek connects this time! This is bowled full and outside the off stump. Logan van Beek hits this hard towards long off for four runs.",
-    },
-    {
-        "Default_Commentary": "43.5: Bilal Khan to Wesley Barresi, 1 run.",
-        "Commentary": "In line with the stumps once again and on a fullish length. Wesley Barresi\u00a0inside edges this through square leg for a single.",
-    },
-    {
-         "Default_Commentary": "42.4: Fayyaz Butt to Saqib Zulfiqar, Four!",
-        "Commentary": "FOUR! Poor delivery from Fayyaz Butt\u00a0\u200b\u200b\u200b\u200band\u00a0Saqib Zulfiqar\u00a0helps himself to his first boundary! This is bowled back of a length and going down leg. Saqib Zulfiqar\u00a0glances this towards fine leg for four runs.",
-    },
-    {
-         "Default_Commentary": "29.4: Fayyaz Butt to Vikramjit Singh, Four!",
-        "Commentary": "FOUR! Vikramjit Singh\u00a0is now one hit away from a well-deserved century! Fayyaz Butt\u00a0comes 'round the wicket and bowls this short, outside off. Vikramjit Singh\u00a0pulls this towards deep mid-wicket and collects four runs. The effort from the fielder is in vain.",
-    }
-]
+
 
 
 @app.route('/')
@@ -44,14 +23,11 @@ def hello_world():
 def get_tasks():
     data = request.get_json()
     # Process the request data here
-    output = getLabels(rows[1])
+    output = getLabels(data)
     # Return a response
     response = {
         'message': 'Received data successfully',
-        'data': {
-            'text': rows[1],
-            'output': output
-        }
+        'data': output
     }
     return jsonify(response)
 
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,64 +1,53 @@
-const FieldingPositionsMap = new Map([
-  [
-    ["first slip", "firstSlip"],
-    ["leg slip", "legSlip"],
-    ["short mid off", "shortMidOff"],
-    ["silly mid off", "sillyMidOff"],
-    ["silly pointgully", "sillyPoint"], //?
-    ["fourth slip", "fourthSlip"],
-    ["third slip", "thirdSlip"],
-    ["second slip", "secondSlip"],
-    ["first slip", "firstSlip"],
-    ["short leg", "shortLeg"],
-    ["silly mid on", "sillyMidOn"],
-    ["point", "point"],
-    ["cover point", "coverPoint"],
-    ["extra cover", "extraCover"],
-    ["mid wicket", "midWicket"],
-    ["deep midwicket", "deepMidWicket"],
-    ["bowlers head", ""], //??
-    ["mid off", "midOff"],
-    ["mid on", "midOn"],
-    ["fly slip", "flySlip"],
-    ["backward point", "backwardPoint"],
-    ["deep coversquare leg", ""], //??
-    ["deep extra cover", "deepExtraCover"],
-    ["long off", "longOff"],
-    ["long on", "longOn"],
-    ["short fine leg", "shortFineLeg"],
-    ["third man", "thirdMan"],
-    ["sweeper", "sweeper"],
-    ["cow corner", "cowCorner"],
-    ["fine leg", "fineLeg"],
-    ["long stop", "longStop"],
-    ["long leg", "longLeg"],
-    ["deep backward square leg", "deepBackwardSquareLeg"],
-    ["deep min wicket", "deepMidWicket"], //??
-    ["backfoot defend", ""], //?
-    ["backfoot defend", ""], //?
-    ["backfoot point", ""], //?
-    ["cover", "cover"],
-    ["cover point", "coverPoint"],
-    ["defend", ""], //?
-    ["edge gully", ""], //?
-    ["extra cover", "extraCover"],
-    ["gully", "gully"],
-    ["leg side", ""], //?
-    ["mid off", "midOff"],
-    ["mid on", "midOn"],
-    ["mid wicket", "midWicket"],
-    ["no connect", ""], //?
-    ["off side", ""], //?
-    ["on side", ""], //?
-    ["point", "point"],
-    ["short cover", ""], //?
-    ["deep cover", "deepCover"],
-    ["short fine leg", "shortFineLeg"],
-    ["square leg", "squareLeg"],
-    ["deep square leg", "deepSquareLeg"],
-    ["pads", ""], //?
-  ],
-]);
+const FieldingPositionsMap = {
+  "first slip": "firstSlip",
+  "leg slip": "legSlip",
+  "short mid off": "shortMidOff",
+  "silly mid off": "sillyMidOff",
+  "fourth slip": "fourthSlip",
+  "third slip": "thirdSlip",
+  "second slip": "secondSlip",
+  "first slip": "firstSlip",
+  "short leg": "shortLeg",
+  "silly mid on": "sillyMidOn",
+  "point": "point",
+  "cover point": "coverPoint",
+  "extra cover": "extraCover",
+  "mid wicket": "midWicket",
+  "deep midwicket": "deepMidWicket",
+  "bowlers head": "", //??
+  "mid off": "midOff",
+  "mid on": "midOn",
+  "fly slip": "flySlip",
+  "backward point": "backwardPoint",
+  "deep cover": "deepCover", 
+  "deep extra cover": "deepExtraCover",
+  "long off": "longOff",
+  "long on": "longOn",
+  "short fine leg": "shortFineLeg",
+  "third man": "thirdMan",
+  "sweeper": "sweeper",
+  "cow corner": "cowCorner",
+  "fine leg": "fineLeg",
+  "long stop": "longStop",
+  "long leg": "longLeg",
+  "deep backward square leg": "deepBackwardSquareLeg",
+  "deep mid wicket": "deepMidWicket",
+  "cover": "cover",
+  "cover point": "coverPoint",
+  "extra cover": "extraCover",
+  "gully": "gully",
+  "mid off": "midOff",
+  "mid on": "midOn",
+  "mid wicket": "midWicket",
+  "off side": "", //?
+  "on side": "", //?
+  "point": "point",
+  "short cover": "cover", //?
+  "deep cover": "deepCover",
+  "short fine leg": "shortFineLeg",
+  "square leg": "squareLeg",
+  "deep square leg": "deepSquareLeg",
+}
 
 const mathcResult = [
   "1 run",
@@ -101,19 +90,27 @@ const UpperZone = [
   "deepMidWicket",
 ];
 
-export const getFieldingPositionClass = (label) => {
-  return FieldingPositionsMap.get([label[0]]);
+export const getFieldingPositionClass = (labels) => {
+    if (!labels && !labels.length) return;
+
+    for (let index = 0; index < labels.length; index++) {
+      const value = FieldingPositionsMap[labels[index ]];
+      if (value) {
+        return value;
+      }
+    }
 };
 
-const isWide = (result) => {
-  return result.indexof("wide") > -1;
+export const isWide = (result) => {
+  return result.indexOf("wide") > -1;
 };
 
-const isNoball = (result) => {
-  return result.indexof("no ball") > -1;
+export const isNoball = (result) => {
+  return result.indexOf("no ball") > -1;
 };
 
-export const getClassByZoneData = (ZAD = "5,46,2") => {
+
+export const getClassByZoneData = (ZAD) => {
   let zad = ZAD.split(",");
   let zone = zad[0],
     distance = zad[2];
@@ -123,3 +120,36 @@ export const getClassByZoneData = (ZAD = "5,46,2") => {
     return LowerZone[zone - 1];
   }
 };
+
+export const getDistance = (ZAD) => {
+    if (!ZAD) return 1;
+    let zad = ZAD.split(",");
+    return parseInt(zad[2])
+}
+
+export const getAnimation = (data) => {
+    const { runs, isNoball, isWide, isWicket} = data;
+    
+    if (isWicket) {
+      return 'ANIMATION_OUT';
+    }
+
+    if (isWide) {
+      return 'ANIMATION_WIDE'
+    }
+
+    if (isNoball) {
+      return 'ANIMATION_WIDE'
+    }
+
+    if (runs) {
+      if(runs == 6) return 'ANIMATION_SIX';
+      if(runs == 4) return 'ANIMATION_FOUR';
+      if(runs == 3) return 'ANIMATION_THREE';
+      if(runs == 2) return 'ANIMATION_TWO';
+      if(runs == 1) return 'ANIMATION_ONE';
+      if(runs == 0) return 'ANIMATION_NO_RUN';
+    }
+
+    return 'ANIMATION_NO_RUN';
+}
