256ba68f421f2f9362168777c7056b2371fd3f76
Neeva Ramoliya <neeva.ramoliya@games24x7.com>
2023-07-09 22:10:34 +0530

    add movement for entire ball

--- src/components/mobileAnimation.js
+++ src/components/mobileAnimation.js
@@ -1,21 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import cx from 'classnames';
 import './mobileAnimation.css';
 import './animations.css';
 
-const MobileAnimation = (data) => {
+const MobileAnimation = (props) => {
+  const { waitingForBall, labelClass, isLeft, isSix, runs, isFour} = props;
+  const [ currentBallState, setCurrentBallState ] = useState('start');
+  const [ umpireFadeIn, setUmpireFadeIn ] = useState('false');
+ 
 
+  useEffect(() => {
+    startBowl()
+  }, [labelClass]) 
+
+  useEffect(() => {
+    console.log(currentBallState)
+  }, [currentBallState])
+
+  const startBowl = () => {
+    setCurrentBallState('deliver');
+    setTimeout(() => { deliverBall()}, 500)
+  }
+
+  const deliverBall = () => {
+    setCurrentBallState('delivered');
+    setTimeout(() => { hitTheBall()}, isSix || isFour ? 1500 : 1000)
+  }
+
+  const hitTheBall = () => {
+    setCurrentBallState(labelClass);
+    setTimeout(() => {ballBack()}, isSix || isFour ? 1500 : 1000)
+  }
+
+  const ballBack = () => {
+    setCurrentBallState("start");
+    setUmpireFadeIn(true)
+    setTimeout(() => {setUmpireFadeIn(false)}, 1500)
+  }
 
   return (
     <>
       <div className="mobileMockup">
         <div className="boundedMobile">
           <div className="boundedWagonWheel">
-            <div class="pitch">
+            <div className="pitch">
               <img src={require("../static/groundBgNew.jpg")} alt="ground" />
-              <div class="ball">
+              <div className= {cx("ball ", currentBallState)}>
                 <img src={require("../static/ballNew.png")} alt="ball" />
               </div>
-              <div class="bat">
+              <div className="bat">
                 <img src={require("../static/batsman.png")} alt="ball" />
               </div>
             </div>
